54ce04f4444c047c6a5119c31a6cc5a427d14554
BenoitSafari <gregory.michalak.192@gmail.com>
2024-09-23 22:43:33 +0100

    release/0.1.0.0: Static files

--- SafariDigital.Api/Program.cs
+++ SafariDigital.Api/Program.cs
@@ -10,23 +10,34 @@ public sealed class Program
     {
         var app = Builder.CreateApp(args);
         app
-            .UseSwagger()
-            .UseSwaggerUI()
             .UseAuthorization()
             .UseCors()
-            .UseRateLimiter();
+            .UseRateLimiter()
+            .UseStaticFiles();
 
-        if (!app.Environment.IsEnvironment("Test"))
-            await ApplyDataMigrationsAsync(app);
+        UseSwaggerPage(app);
+        await ApplyDataMigrationsAsync(app);
 
         app.MapControllers().RequireRateLimiting("Default");
         await app.RunAsync();
     }
 
-    private static async Task ApplyDataMigrationsAsync(IHost app)
+    private static async Task ApplyDataMigrationsAsync(WebApplication app)
     {
+        if (app.Environment.IsEnvironment("Test")) return;
         using var scope = app.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<SafariDigitalContext>();
         await context.Database.MigrateAsync();
+    }
+
+    private static void UseSwaggerPage(WebApplication app)
+    {
+        if (!app.Environment.IsEnvironment("Development")) return;
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "SafariDigital API V1");
+            c.RoutePrefix = "swagger";
+        });
     }
 }
