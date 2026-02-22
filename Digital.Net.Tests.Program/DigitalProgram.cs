9a2e5cd406b15c795a1431053add0a19881f580c
BenoitSafari <gregory.michalak.192@gmail.com>
2026-02-22 23:32:31 +0100

    Fixed Integration test routes not found

--- Digital.Net.Tests.Program/DigitalProgram.cs
+++ Digital.Net.Tests.Program/DigitalProgram.cs
@@ -1,6 +1,9 @@
 using System.Threading.Tasks;
+using Digital.Net.Api.Authentication.Filters;
 using Digital.Net.Api.Sdk;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace Digital.Net.Tests.Program;
 
@@ -12,9 +15,31 @@ public sealed class DigitalProgram
             .AddDigitalSdk()
             .Build();
 
-        app
-            .UseDigitalSdk();
+        app.UseDigitalSdk();
+        MapTestEndpoints(app);
+
 
         await app.RunAsync();
+    }
+
+    public static IEndpointRouteBuilder MapTestEndpoints(IEndpointRouteBuilder app)
+    {
+        var controller = app
+            .MapGroup("/test/authentication")
+            .WithTags("Test");
+
+        controller
+            .MapGet("/jwt", () => Results.Ok())
+            .RequireAuthentication(AuthorizeType.Jwt);
+
+        controller
+            .MapGet("/api-key", () => Results.Ok())
+            .RequireAuthentication(AuthorizeType.ApiKey);
+
+        controller
+            .MapGet("/any", () => Results.Ok())
+            .RequireAuthentication(AuthorizeType.Jwt | AuthorizeType.ApiKey);
+
+        return app;
     }
 }
